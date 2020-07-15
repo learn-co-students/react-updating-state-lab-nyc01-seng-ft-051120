d6e0cc4e7f376c1954db5479022cfb8f8e4de359
callas4ever <thetseringnorbu@gmail.com>
2020-07-14 21:54:31 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,14 @@
-// Code DigitalClicker Component Here
+import React from 'react'
+class DigitalClicker extends React.Component{
+    state={timesClicked: 0}
+    clcikHandler=()=>{
+        this.setState({timesClicked: this.state.timesClicked+1})
+    }
+
+    render(){
+        return(
+        <button onClick={this.clcikHandler} >{this.state.timesClicked}</button>
+        )
+    }
+}
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,30 @@
-// Code YouTubeDebugger Component Here
+import React from 'react'
+
+class YouTubeDebugger extends React.Component{
+    state={
+        errors: [],
+        user: null,
+        settings: {
+          bitrate: 8,
+          video: {
+            resolution: '1080p'
+          }
+        }
+      }
+      bitrateHandler=()=>{
+          this.setState({settings: {...this.state.settings, bitrate: 12}})
+      }
+      resolutionHandler=()=>{
+          this.setState({settings: { ...this.state.settings, video:{...this.state.settings.video, resolution: '720p'}}})
+      }
+    render(){
+        return(
+            <div>
+                <button className='bitrate'onClick={this.bitrateHandler}>Tsering</button>
+                <button className='resolution' onClick={this.resolutionHandler}>Norbu</button>
+            </div>
+        
+        )
+    }
+}
+export default YouTubeDebugger
